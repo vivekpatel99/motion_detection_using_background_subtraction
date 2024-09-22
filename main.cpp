11bfad94da1b4cce4693c2de8be3d0dacb6cb393
vivekpatel99 <vivpatel99@gmail.com>
2024-09-22 11:51:16 +0200

    issue with clearing first frame

--- main.cpp
+++ main.cpp
@@ -1,92 +1,91 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <unistd.h>
-// https://docs.opencv.org/4.x/d7/d9f/tutorial_linux_install.html
+
 using namespace std;
 
 int main()
 {
-
-    cv::VideoCapture cap;
-    int deviceID = 0;        // 0 = open default camera
-    int apiID = cv::CAP_ANY; // 0 = autodetect default API
-
-    cv::Mat firstFrame;
-    cv::Mat frame;
-    cv::Mat grayFrame;
-    cv::Mat imgBlur;
-    cv::Mat frameDelta;
-    cv::Mat thresh;
-    int x, y, w, h;
-
-    vector<vector<cv::Point>> contours;
-    int MIN_AREA = 500;
-
-    cv::Mat currentFrame;
-    // open selected camera using selected API
-    cap.open(deviceID, apiID);
-    // check if we succeeded
+    int CAM_NUM = 0; // Replace with your camera number
+    cv::VideoCapture cap(CAM_NUM);
     if (!cap.isOpened())
     {
         cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
-    //--- GRAB AND WRITE LOOP
-    cout << "Start grabbing" << endl
-         << "Press any key to terminate" << endl;
+
+    cv::Mat firstFrame;
+    cv::Mat frame;
+    cv::Mat gray;
+    cv::Mat frameDelta;
+    cv::Mat thresh;
+    int MIN_AREA = 500;
+
+    usleep(2000000); // Sleep for 2 seconds
+
     while (true)
     {
         cap.read(frame);
-        // if the first stream is not initialized, store it for reference	cap.read(firstFrame);
         if (frame.empty())
         {
-            cout << "Could not open or find the image!\n"
-                 << endl;
-            return 1;
+            cout << "ERROR: No frame available !!\n";
+            break;
         }
-        // color has no bearing on motion detection algorithm
-        cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
 
-        // to smooth the image and remove noise(if not then could throw algorithm off)
-        // smothing avarage pixel intensities across an 21x21 region
-        cv::GaussianBlur(grayFrame, imgBlur, cv::Size(21, 21), 0, 0);
+        // Resize the image to have less processing time
+        cv::resize(frame, frame, cv::Size(500, frame.rows * 500 / frame.cols));
+        // Convert to grayscale
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        // Smooth the image to remove noise
+        cv::GaussianBlur(gray, gray, cv::Size(21, 21), 0);
 
-        // if the first stream is not initialized, store it for reference
+        // If the first frame is not initialized, store it for reference
         if (firstFrame.empty())
         {
-            firstFrame = grayFrame.clone(); // Store the first frame in grayscale
+            firstFrame = gray.clone();
             continue;
         }
-        // compute the absolute difference between the current frame and firstFrame
-        cv::absdiff(firstFrame, imgBlur, frameDelta);
-        cv::threshold(frameDelta, thresh, 10.0, 255.0, cv::THRESH_BINARY);
 
-        // // dilate the threshold image to fill in holes, then find contours on the thresholded image
-        // // apply background substraction
+        // Compute the absolute difference between the current frame and firstFrame
+        cv::absdiff(firstFrame, gray, frameDelta);
+        cv::threshold(frameDelta, thresh, 25, 255, cv::THRESH_BINARY);
+
+        // Dilate the threshold image to fill in holes, then find contours on the thresholded image
         cv::dilate(thresh, thresh, cv::Mat(), cv::Point(-1, -1), 2);
 
+        vector<vector<cv::Point>> contours;
         cv::findContours(thresh.clone(), contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-        vector<cv::Rect> boundRect(contours.size());
-        if (contours.empty())
-            continue;
 
+        // Looping for contours
         for (size_t i = 0; i < contours.size(); i++)
         {
             if (cv::contourArea(contours[i]) < MIN_AREA)
                 continue;
-            // get bounding box from countour
-            boundRect[i] = cv::boundingRect(contours[i]);
-            // draw bounding box
-            cv::rectangle(frame, boundRect[i], cv::Scalar(0, 255, 0), 2);
+
+            cv::Moments M = cv::moments(contours[i]);
+            int cX = int(M.m10 / M.m00);
+            int cY = int(M.m01 / M.m00);
+
+            // Get bounding box from contour
+            cv::Rect boundingBox = cv::boundingRect(contours[i]);
+
+            // Draw bounding box
+            cv::rectangle(frame, boundingBox, cv::Scalar(0, 255, 0), 2);
+            cv::circle(frame, cv::Point(cX, cY), 7, cv::Scalar(0, 0, 255), -1);
         }
 
-        cv::imshow("firstFrame", frame);
+        cv::imshow("Original", frame);
+        cv::imshow("Threshold", thresh);
+        cv::imshow("FrameDelta", frameDelta);
 
-        if (cv::waitKey(1) >= 0)
+        if (cv::waitKey(1) == 'q')
         {
             break;
         }
+        firstFrame.empty();
     }
+
     cap.release();
+    cv::destroyAllWindows();
     return 0;
 }
